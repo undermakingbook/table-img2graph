225a976f449c26c01617f3100d875e05f627619d
yamamoto-yuta <automatuX78@gmail.com>
2020-10-21 04:42:29 +0900

    [Add] Add argparser

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 from img2graph import Img2graph
 
+import argparse
+
 if __name__ == '__main__':
+
+    # arguments
+    parser = argparse.ArgumentParser(description='Converting table image from scanned pdf data to graph of cells.')
+    parser.add_argument('image_path', help='image path')
+    args = parser.parse_args()
+
+    # img2graph
     i2g = Img2graph('tesseract')
-    i2g.execute('/home/res/sample_copy.png')
+    i2g.execute(args.image_path)
