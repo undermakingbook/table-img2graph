fd1efffc289adc0779a51e0b116f8a5a59ab2b59
yamamoto-yuta <automatuX78@gmail.com>
2020-11-25 05:28:19 +0900

    [Add] Add command line arg: '--is_dir'

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
+import argparse
+from glob import glob
+from tqdm.auto import tqdm
+
 from table_img2graph import TableImg2graph
 
-import argparse
+def correct_last_slash(path):
+    if path[-1] == '/':
+        return path
+    else:
+        return path + '/'
+
+def make_path_list(arg, is_dir):
+    if is_dir:
+        return glob(correct_last_slash(arg) + '*')
+    else:
+        return [arg]
 
 if __name__ == '__main__':
 
@@ -9,13 +23,19 @@ if __name__ == '__main__':
     parser.add_argument('-a', '--api', default='tesseract')
     parser.add_argument('-c', '--convert')
     parser.add_argument('-e', '--extract')
+    parser.add_argument('-d', '--is_dir', action='store_true')
     args = parser.parse_args()
 
     # table-img2graph
     ti2g = TableImg2graph(args.api)
     if args.convert:
-        ti2g.convert_table_img2graph(args.convert)
+        lst_path = make_path_list(args.convert, args.is_dir)
+        for path in lst_path:
+            ti2g.convert_table_img2graph(path)
     elif args.extract:
-        ti2g.extract_table(args.extract)
+        lst_path = make_path_list(args.extract, args.is_dir)
+        for path in tqdm(lst_path):
+            ti2g.extract_table(path)
     else:
         print('Arguments Error!')
+
